38fa90990a915ec4b18e3e838ebcc8df9d027ae0
ptphong131202 <ptphonggithub@gmail.com>
2023-04-10 08:00:17 +0700

    add file do an

---
+++ note.md
@@ -0,0 +1,7 @@
+Project:
+    partials
+        - header: logo, nav, input search
+        - slider: nằm ở phần trang chủ, dùng carousel để chay ảnh
+        - footer: 
+    public
+    
---
+++ public/about.php
@@ -0,0 +1,41 @@
+<?php
+include_once "C:/xampp/apps/project/bootstrap.php";
+session_start();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <title>About | Shop Hoàng Thượng</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/base.css">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+	<?php include('../partials/header.php') ?>
+    <p class="paddingtop"></p>    
+
+	<!-- Main Page Content -->
+	    <h2 class="text-center my-3">Về chúng tôi</h2>
+        <hr/>
+        <div class="row my-4">
+            <div class="col"><img class="w-100 h-80" alt="ve-chung-toi" src="img/about/about.jpg" /></div>
+            <div class="col lh-lg fst-italic">
+                <p>Với sự cố gắng không ngừng cũng như hiểu được nhu cầu ăn uống của mọi người ở hiện tại. Chúng tôi đã cho ra đời hàng loạt những loại thức uống mới, thơm ngon để phục vụ. Đối với cửa hàng, khách hàng luôn được đặt lên hàng đầu dù có ở bất kỳ địa vị, bằng cấp nào. Vậy còn chần chờ gì nữa, hãy đăng nhập và đặt cho mình những món nước thơm ngon nhất nào!!! </p>
+                <p>Không chỉ đa dạng về mẫu mã, chúng tôi còn luôn cập nhật và thử nghiệm các loại thức uống mới để đáp ứng được sự mong đợi của khách hàng. Với đội ngũ nhân viên nhiệt tình và chuyên nghiệp, chúng tôi sẵn sàng phục vụ bạn mọi lúc mọi nơi.</p>
+                <p>Đặc biệt, chúng tôi còn có chính sách giá cả hợp lý và ưu đãi dành cho khách hàng thân thiết. Hãy đến với chúng tôi để thưởng thức những loại thức uống tuyệt vời và tận hưởng những khoảnh khắc đáng nhớ cùng gia đình và bạn bè.</p>
+            </div>
+        </div>
+
+
+    <script src="js/dungchung1.js"></script>
+	<?php include('../partials/footer.php') ?>
+	<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.5/dist/umd/popper.min.js" integrity="sha384-Xe+8cL9oJa6tN/veChSP7q+mnSPaj5Bcu9mPX5F5xIGE0DVittaqT5lorf0EI7Vk" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.min.js" integrity="sha384-ODmDIVzN+pFdexxHEHFBQH3/9/vQ9uori45z4JjnFsRydbmQbmL5t1tQ0culUzyK" crossorigin="anonymous"></script>
+	
+</body>
+
+</html>
